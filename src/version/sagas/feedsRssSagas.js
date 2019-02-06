abade3ad7ece5843fb3912d23c2670d056ceeb40
bogdan_scher <bsc@piogroup.com>
2019-02-06 14:39:01 +0200

    new feeds parcer

--- src/version/sagas/feedsRssSagas.js
+++ src/version/sagas/feedsRssSagas.js
@@ -1,11 +1,9 @@
 import { takeEvery, put } from 'redux-saga/effects';
-import { get } from "lodash";
-import * as rssParser from 'react-native-rss-parser';
 
 import { FEEDS_RSS_ACTION, feedsRssAction } from "../actions/feedsRssAction";
+import { getRssFeedsListFromXML } from "../plugins/Feeds/rss-helper";
 
 const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
-const FEEDS_NUMBER = 4;
 
 export default takeEvery(FEEDS_RSS_ACTION.REQUEST, function*(action) {
     const feedsUrl = action.rssFeedUrl;
@@ -14,19 +12,9 @@ export default takeEvery(FEEDS_RSS_ACTION.REQUEST, function*(action) {
     try {
         const result = yield fetch(url)
             .then(res => res.text())
-            .then(responseData => rssParser.parse(responseData))
-            .then(res => {
-                const { items } = res;
-                let feeds = [];
-                for (let i = 0; i < FEEDS_NUMBER; i++) {
-                    let item = items[i];
-                    feeds.push({
-                        title: get(item, 'title', null),
-                        link: get(item, 'id', null),
-                    })
-                }
-                return feeds;
-            });
+            .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+            .then(responseData => getRssFeedsListFromXML(responseData))
+            .then(res => res);
         yield put(feedsRssAction.success({
             [sourceId]: result,
         }))
