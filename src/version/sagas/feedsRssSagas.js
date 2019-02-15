fa4bfbf41a5971e35ba399f762eac95d23e260f0
bogdan_scher <bsc@piogroup.com>
2019-02-15 13:40:01 +0200

    notes fixing

--- src/version/sagas/feedsRssSagas.js
+++ src/version/sagas/feedsRssSagas.js
@@ -6,13 +6,12 @@ import { getRssFeedsListFromXML } from "../plugins/Feeds/rss-helper";
 export default takeEvery(FEEDS_RSS_ACTION.REQUEST, function*(action) {
     const feedsUrl = action.rssFeedUrl;
     const sourceId = action.sourceId;
-    const url = "https://cors-anywhere.herokuapp.com/" + feedsUrl.replace("http://", "").replace("https://", "");
+    const url = "https://cors-anywhere.herokuapp.com/" + encodeURI(feedsUrl);
     try {
         const result = yield fetch(url)
             .then(res => res.text())
             .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
-            .then(responseData => getRssFeedsListFromXML(responseData))
-            .then(res => res);
+            .then(responseData => getRssFeedsListFromXML(responseData));
         yield put(feedsRssAction.success({
             [sourceId]: result,
         }))
