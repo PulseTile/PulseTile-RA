a597bede46d7c1d5022c43ab33f71278038ca0a9
bogdan_scher <bsc@piogroup.com>
2019-03-27 16:43:55 +0200

    custom sagas

--- src/core/sagas/httpErrorSagas.js
+++ src/core/sagas/httpErrorSagas.js
@@ -1,10 +1,22 @@
 import get from "lodash/get";
-import { CRUD_GET_LIST_FAILURE } from "react-admin";
+import {
+    CRUD_GET_LIST_FAILURE,
+    CRUD_GET_ONE_FAILURE,
+    CRUD_CREATE_FAILURE,
+    CRUD_UPDATE_FAILURE,
+} from "react-admin";
 import { put, takeEvery } from "redux-saga/effects";
 
 import { httpErrorAction } from '../actions/httpErrorAction';
 
-export default takeEvery(CRUD_GET_LIST_FAILURE, function* (action) {
+const actions = [
+    CRUD_GET_LIST_FAILURE,
+    CRUD_GET_ONE_FAILURE,
+    CRUD_CREATE_FAILURE,
+    CRUD_UPDATE_FAILURE,
+];
+
+export default takeEvery(actions, function* (action) {
     const error = get(action, 'error', null);
     const errorTrim = error.replace('Error:', '').trim();
     const errorArray = errorTrim.split('|');
