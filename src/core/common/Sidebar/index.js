69ac236dce093fb5dd0d1ab9fa2e534855213b38
bogdan_scher <bsc@piogroup.com>
2019-04-08 17:32:22 +0300

    mobile menu

--- src/core/common/Sidebar/index.js
+++ src/core/common/Sidebar/index.js
@@ -1,14 +1,14 @@
-import React, { createElement } from 'react';
+import React, { Component } from 'react';
 import get from "lodash/get";
 
 import { withRouter } from 'react-router-dom';
-import { MenuItemLink, Sidebar, getResources, Responsive } from 'react-admin';
+import { Sidebar, getResources, Responsive, setSidebarVisibility } from 'react-admin';
 import { connect } from 'react-redux';
 
 import { withStyles } from '@material-ui/core/styles';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircle } from '@fortawesome/free-solid-svg-icons'
 
+import MobileMenu from "./MobileMenu";
+import MenuItems from "./MenuItems";
 import { getMenuItems } from "./getMenuType";
 
 const styles = theme => ({
@@ -44,54 +44,32 @@ const styles = theme => ({
     },
 });
 
-const MenuItems = ({ classes, menuItems, currentList, onMenuClick }) => {
-    return (
-        <div className={classes.menuBlock} role="menubar">
-            {menuItems.map((item, key) => (
-                    <MenuItemLink
-                        key={key}
-                        className={(currentList === item.url) ? classes.menuItemSelected : classes.menuItem}
-                        to={item.url}
-                        primaryText={item.label}
-                        leftIcon={(currentList === item.url) ? <FontAwesomeIcon icon={faCircle} size="xs" />  : null}
-                        onClick={onMenuClick}
-                        selected={currentList === item.url}
-                        aria-label={item.label}
-                        role="menuitem"
-                    />
-                )
-            )}
-        </div>
-    )
-};
-
 /**
  * This component returns custom menu
  *
  * @author Bogdan Shcherban <bsc@piogroup.net>
- * @param {shape} props
- * @constructor
  */
-const CustomSidebar = props => {
-    const { classes, isSidebarOpen, onMenuClick, location } = props;
+const CustomSidebar = ({ classes, isSidebarOpen, onMenuClick, location }) => {
     const currentPathname = get(location, 'pathname', null);
     const pathNameArray = currentPathname.split('/');
     const currentList = '/' + pathNameArray[1];
     const menuItems = getMenuItems(currentPathname);
-    if (!isSidebarOpen) {
-        return null;
-    }
     return (
         <Responsive
             small={
-                <div className={classes.mobileSidebar}>
-                    <MenuItems classes={classes} menuItems={menuItems} currentList={currentList} onMenuClick={onMenuClick} />
-                </div>
+                isSidebarOpen ? null : <MobileMenu
+                    classes={classes}
+                    menuItems={menuItems}
+                    currentList={currentList}
+                    onMenuClick={onMenuClick}
+                />
             }
             medium={
-                <Sidebar className={classes.sidebarBlock}>
-                    <MenuItems classes={classes} menuItems={menuItems} currentList={currentList} onMenuClick={onMenuClick} />
-                </Sidebar>
+                isSidebarOpen ?
+                    <Sidebar className={classes.sidebarBlock}>
+                        <MenuItems classes={classes} menuItems={menuItems} currentList={currentList} onMenuClick={onMenuClick} />
+                    </Sidebar>
+                    : null
             }
         >
         </Responsive>
@@ -103,4 +81,4 @@ const mapStateToProps = state => ({
     isSidebarOpen: state.admin.ui.sidebarOpen,
 });
 
-export default withRouter(connect(mapStateToProps)(withStyles(styles)(CustomSidebar)));
+export default withRouter(connect(mapStateToProps, null)(withStyles(styles)(CustomSidebar)));
