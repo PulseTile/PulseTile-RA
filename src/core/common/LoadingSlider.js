af7c18e4cd32ba9ff90843190b5865d5bbd1d773
BogdanScherban <bsc@piogroup.net>
2019-05-14 18:53:50 +0300

    loader

--- src/core/common/LoadingSlider.js
+++ src/core/common/LoadingSlider.js
@@ -111,7 +111,7 @@ const styles = {
             left: 40,
             borderRadius: "50%",
             border: "10px solid #000",
-            borderColor: "#3596f4 transparent #3596f4 transparent",
+            borderColor: "#1d3797 transparent #1d3797 transparent",
             WebkitAnimation: "rings 1s linear infinite",
             animation: `rings 1s linear infinite`,
         },
