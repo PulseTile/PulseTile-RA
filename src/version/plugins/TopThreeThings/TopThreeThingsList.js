f9ee6f2e4ff0e9542f53169bd24ffa41e86a5294
BogdanScherban <bsc@piogroup.net>
2019-06-06 20:04:06 +0300

    top3 and vac with custom lists

--- src/version/plugins/TopThreeThings/TopThreeThingsList.js
+++ src/version/plugins/TopThreeThings/TopThreeThingsList.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Route } from "react-router";
 import { DateField, TextField } from "react-admin";
 
 import ListTemplate from "../../../core/common/ResourseTemplates/ListTemplate";
 import TopThreeThingsCreate from "./TopThreeThingsCreate";
 import TopThreeThingsEdit from "./TopThreeThingsEdit";
 import TopThreeThingsShow from "./TopThreeThingsShow";
+import DatagridRow from "./fragments/DatagridRow";
 
 /**
  * This component returns block with Top Three Things list
@@ -23,6 +23,8 @@ const TopThreeThingsList = ({ classes, ...rest }) => (
       show={TopThreeThingsShow}
       resourceUrl="top3Things"
       title="Top Three Things"
+      CustomRow={DatagridRow}
+      isCustomDatagrid={true}
       {...rest}
     >
         <DateField source="dateCreated" label="Date created"  />
